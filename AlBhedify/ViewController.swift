9146c5b2536781044ee327b999ce3df8b01e1ed8
Marcus Ekelund <marcus.ekelund@gmail.com>
2016-03-02 11:36:03 +0100

    Initial Commit

---
+++ AlBhedify/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AlBhedify
+//
+//  Created by Marcus Ekelund on 2016-03-02.
+//  Copyright © 2016 Marcus Ekelund. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ AlBhedifyTests/AlBhedifyTests.swift
@@ -0,0 +1,36 @@
+//
+//  AlBhedifyTests.swift
+//  AlBhedifyTests
+//
+//  Created by Marcus Ekelund on 2016-03-02.
+//  Copyright © 2016 Marcus Ekelund. All rights reserved.
+//
+
+import XCTest
+@testable import AlBhedify
+
+class AlBhedifyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
