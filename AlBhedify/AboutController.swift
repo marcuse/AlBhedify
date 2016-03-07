c050845f790268a3afa615ac97eb7250731d9f5b
Marcus Ekelund <marcus.ekelund@gmail.com>
2016-03-07 15:45:28 +0100

    Rudimentary About view added.

---
+++ AlBhedify/AboutController.swift
@@ -0,0 +1,15 @@
+//
+//  AboutController.swift
+//  AlBhedify
+//
+//  Created by Marcus Ekelund on 2016-03-07.
+//  Copyright © 2016 Marcus Ekelund. All rights reserved.
+//
+
+import UIKit
+
+class AboutController: UIViewController {
+    @IBAction func doneClicked(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+}
