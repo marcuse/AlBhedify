5757db00bfa95ccab429abd54c31f92520daf052
Marcus Ekelund <marcus.ekelund@gmail.com>
2016-03-14 17:42:31 +0100

    Redesigned and a bit mor informative About view.

--- AlBhedify/AboutController.swift
+++ AlBhedify/AboutController.swift
@@ -9,6 +9,16 @@
 import UIKit
 
 class AboutController: UIViewController {
+    @IBOutlet weak var AboutContent: UIWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let url = NSBundle.mainBundle().URLForResource("about", withExtension: "html")
+        let request = NSURLRequest(URL: url!)
+        AboutContent.loadRequest(request)
+    }
+    
     @IBAction func doneClicked(sender: AnyObject) {
         self.dismissViewControllerAnimated(true, completion: nil)
     }
